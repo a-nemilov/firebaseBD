a49550767cb6fd306434d9a4e17c34e1c901b4b1
Andrey Nemilov <andrey.nemilov@pandadoc.com>
2019-03-20 11:08:10 +0300

    all

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ReactDOM from "react-dom";
+import App from "./components/App.js";
+
+ReactDOM.render(<App />, document.getElementById("root"));
